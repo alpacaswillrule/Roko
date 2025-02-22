d4ab1136bd0377d521a2f5f7c3762fd6a2dc7294
johanvlassak <johan.vlassak@gmail.com>
2025-02-22 18:48:26 -0500

    cleaning up and preping test.py for frontend

--- ui/test.py
+++ ui/test.py
@@ -1,20 +1,28 @@
 from transformers import AutoTokenizer
 from petals import AutoDistributedModelForCausalLM
 
-# Initialize the model and tokenizer
-print("check1")
-model_name = "bigscience/bloom-560m"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-print("check2")
-model = AutoDistributedModelForCausalLM.from_pretrained(model_name)
-print("check3")
-# Prepare input text
-text = "Write a short story about a cat sitting on the floor:"
-inputs = tokenizer(text, return_tensors="pt", max_length=1024, padding=True)["input_ids"]
-print("check4")
-inputs = tokenizer("A cat sat", return_tensors="pt")["input_ids"]
-print("check5")
-outputs = model.generate(inputs, max_new_tokens=60)
-print("check6")
-decoded_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(decoded_text)
+def execute(input_text, starter_sent):
+    model_name = "bigscience/bloom-560m"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoDistributedModelForCausalLM.from_pretrained(model_name)
+    text = input_text
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, padding=True)["input_ids"]
+    inputs = tokenizer("starter_sent", return_tensors="pt")["input_ids"]
+    outputs = model.generate(inputs, max_new_tokens=60)
+    decoded_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(decoded_text)
+    return decoded_text
+
+
+if __name__ == "__main__":
+    # Initialize the model and tokenizer
+    model_name = "bigscience/bloom-560m"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoDistributedModelForCausalLM.from_pretrained(model_name)
+    print("check3")
+    text = "Write a short story about a cat sitting on the floor:"
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, padding=True)["input_ids"]
+    inputs = tokenizer("A cat sat", return_tensors="pt")["input_ids"]
+    outputs = model.generate(inputs, max_new_tokens=60)
+    decoded_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(decoded_text)
